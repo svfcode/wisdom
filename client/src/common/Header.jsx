6f4a3a0e8c013145b6463ea26f3e3c2aeb3cf21b
klimovcode <klimovcode@mail.ru>
2020-12-28 20:06:23 +0500

    layout Header

---
+++ client/src/common/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-sm navbar-dark fixed-top bg-dark">
+        <a href="/" className="navbar-brand">KlimovCode</a>
+        <div className="collapse navbar-collapse d-flex justify-content-between">
+          <span className="text-light">Видение всегда предшествует осуществлению</span>
+          <button type="button" className="btn btn-primary">Связаться</button>
+        </div>
+
+      </nav>
+    </header>
+  )
+}
