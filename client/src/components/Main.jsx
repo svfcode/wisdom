fb62ec8827d58aef28c510146df2941bd31f17ba
klimovcode <klimovcode@mail.ru>
2020-12-30 13:23:21 +0500

    start rewrite architecture

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
-import Carousel from './Carousel'
+import Card from './Card'
 
 export default () => {
   return (
     <main className="mt-5">
 
 
-      <div className="container">
-        <div className="row">
+      <div className="container text-center">
 
           <div class="card text-center">
             <div class="card-header">Обучение</div>
@@ -15,18 +14,18 @@ export default () => {
               <h5 class="card-title">База знаний</h5>
               <p class="card-text">
                 Перечисленны информационные материалы на которых основана моя компетенция.
-                Чтобы много иметь надо много знать, для этого нужно много учиться.
+                
               </p>
 
-              <Carousel />
+              <Card />
 
               <a href="#" class="btn btn-primary">Посмотреть полный список</a>
             </div>
-            <div class="card-footer text-muted">Обновленно 26.12.20</div>
+            <div class="card-footer text-muted">Чтобы много иметь надо много знать, для этого нужно много учиться.</div>
           </div>
 
         </div>
-      </div>
+     
 
     </main>
   )
