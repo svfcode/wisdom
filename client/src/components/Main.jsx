32e39d784f454abfe62281416a7023c902c05785
klimovcode <klimovcode@mail.ru>
2021-01-02 00:20:22 +0500

    add text content for each card

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -5,12 +5,25 @@ export default () => {
   return (
     <main className="mt-5">
       <div className="container text-center d-flex flex-wrap justify-content-center">
-        <Card name="study" cardName="Обучение"
+
+        <Card name="study"
+          cardName="Обучение"
           cardQuot="Чтобы много иметь надо много знать, для этого нужно много учиться."
           cardTitle="База знаний"
           cardDescription="Перечисленны информационные материалы на которых основана моя компетенция." />
-        <Card name="experience" />
-        <Card name="skills" />
+
+        <Card name="experience"
+          cardName="Опыт"
+          cardQuot="Самый хороший учитель в жизни — опыт.
+          Берет, правда, дорого, но объясняет доходчиво."
+          cardTitle="Выполненые работы"
+          cardDescription="Результаты труда - показатель состоятельности специалиста." />
+
+        <Card name="skills"
+          cardName="Тенологии"
+          cardQuot="Каждой работе - свой инструмент."
+          cardTitle="Технологии которыми владею"
+          cardDescription="Перечислен технологический стек." />
       </div>
     </main>
   )
