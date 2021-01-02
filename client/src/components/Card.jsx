977452cf28affcb826b481dd4674793bc61645b3
klimovcode <klimovcode@mail.ru>
2021-01-02 16:09:00 +0500

    add logic for modal

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -3,17 +3,38 @@ import Slider from './Slider'
 
 export default (props) => {
   return (
-    <div className="card text-center m-2">
-      <div className="card-header"> {props.cardName} </div>
-      <div className="card-body">
-        <h5 className="card-title"> {props.cardTitle} </h5>
-        <p className="card-text"> {props.cardDescription} </p>
-
-        <Slider name={props.name} imgs={props.imgs} />
-
-        <a href="#" className="btn btn-primary mt-3">Посмотреть полный список</a>
+    <>
+      <div id={props.name} className="modal fade">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">{props.cardTitle}</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <p>{props.cardDescription}</p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Свернуть</button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className="card-footer text-muted"> {props.cardQuot} </div>
-    </div>
+
+      <div className="card text-center m-2">
+        <div className="card-header"> {props.cardName} </div>
+        <div className="card-body">
+          <h5 className="card-title"> {props.cardTitle} </h5>
+          <p className="card-text"> {props.cardDescription} </p>
+
+          <Slider name={props.name} imgs={props.imgs} />
+
+          <a href="#" className="btn btn-primary mt-3" data-bs-toggle="modal" data-bs-target={'#' + props.name}>
+            Посмотреть полный список
+          </a>
+        </div>
+        <div className="card-footer text-muted"> {props.cardQuot} </div>
+      </div>
+    </>
   )
 }
