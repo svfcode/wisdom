9b9a9620cf3dad2c2ca970bbec2eb78b3c739ad1
klimovcode <klimovcode@mail.ru>
2021-01-03 17:37:47 +0500

    add description

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import Header from './common/Header'
 import Footer from './common/Footer'
 import Main from './components/Main'
 import Contacts from './common/Contacts'
+import Description from './common/Description'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Description />
       <Main />
       <Contacts />
       <Footer />
