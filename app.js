d517edff027760757d46dfc2301a997528d8f225
klimovcode <klimovcode@mail.ru>
2021-01-04 22:34:19 +0500

    prepare for deploy

--- app.js
+++ app.js
@@ -13,6 +13,18 @@ app.options('*', cors())
 var db
 
 if (process.env.NODE_ENV === 'production') {
+  app.post('/api', async (req, res) => {
+    customer = req.body
+    await db.collection('msgs').insertOne(customer, (err, result) => {
+      if (err) {
+        console.log(err)
+        return res.sendStatus(500)
+      }
+    })
+    let msgs = await db.collection('msgs').find().toArray()
+    res.status(201).json({ msg: 'All OK!', msgs })
+  })
+
   app.use('/', express.static(path.join(__dirname, 'client', 'build')))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
